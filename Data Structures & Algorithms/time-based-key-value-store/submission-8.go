eb240d71216ce25d01ceac7f1bd408702905fd51
rye <59441604+rye-ndt@users.noreply.github.com>
2026-08-13 10:18:01 +0700

    Add: time-based-key-value-store - submission-8

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-8.go
@@ -0,0 +1,60 @@
+type TimeMap struct {
+	data map[string]map[int]string
+	added map[string][]int
+}
+
+func Constructor() TimeMap {
+	return TimeMap{
+		data: map[string]map[int]string{},
+		added: map[string][]int{},
+	}
+}
+
+func (this *TimeMap) Set(key string, value string, timestamp int) {
+	if _, found := this.data[key]; !found {
+		this.data[key] = map[int]string{}
+		this.added[key] = []int{}
+	}
+
+	this.data[key][timestamp] = value
+	this.added[key] = append(this.added[key], timestamp)
+}
+
+func (this *TimeMap) Get(key string, timestamp int) string {
+
+	keyAdded, found := this.added[key]
+	if !found{
+		return ""
+	}
+
+	// 2
+	l, r := 0, len(keyAdded)-1
+
+	biggest := -1
+
+	for l <= r {
+		m := (l + r) / 2
+
+
+
+		if timestamp > keyAdded[m] {
+			biggest = max(biggest, keyAdded[m])
+			l = m + 1
+		} else if timestamp < keyAdded[m] {
+			if timestamp < keyAdded[l] { 
+				break
+			}
+			biggest = max(biggest, keyAdded[l])
+			r = m - 1
+		} else {
+			biggest = keyAdded[m]
+			break
+		}
+	}
+
+	if biggest == -1 {
+		return ""
+	}
+
+	return this.data[key][biggest]
+}
