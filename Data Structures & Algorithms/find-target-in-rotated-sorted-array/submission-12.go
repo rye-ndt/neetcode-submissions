c24d7016b7b30d525abf7fea62d862f0ed1efc01
rye <59441604+rye-ndt@users.noreply.github.com>
2026-08-13 09:47:22 +0700

    Add: find-target-in-rotated-sorted-array - submission-12

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-12.go
@@ -0,0 +1,27 @@
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		m := (l + r) / 2
+
+		if nums[m] == target { return m }
+
+		switch {
+			case nums[l] < nums[m] || nums[r] < nums[m]: 
+				if target >= nums[l] && target < nums[m] {
+					r = m - 1
+				} else {
+					l = m + 1
+				}
+			case nums[l] > nums[m] || nums[r] > nums[m]:
+				if target > nums[m] && target <= nums[r] {
+					l = m + 1
+				} else {
+					r = m - 1
+				}
+			default: return -1
+		}
+	}
+
+	return -1
+}
