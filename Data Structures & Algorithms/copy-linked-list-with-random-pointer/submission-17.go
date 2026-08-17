51260f310a69b0061aa0b6c49975b95310336c58
rye <59441604+rye-ndt@users.noreply.github.com>
2026-08-17 10:55:05 +0700

    Add: copy-linked-list-with-random-pointer - submission-17

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-17.go
@@ -0,0 +1,17 @@
+func copyRandomList(h *Node) *Node {
+	mapper := map[*Node]*Node{}
+
+	for i := h; i != nil; i = i.Next {
+		mapper[i] = &Node{ Val: i.Val }
+	}
+
+	result := mapper[h]
+
+	for i := result; h != nil; h = h.Next {
+		i.Next = mapper[h.Next]
+		i.Random = mapper[h.Random]
+		i = i.Next
+	}
+
+	return result
+}
