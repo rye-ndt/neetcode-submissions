b0533cb4b5d11dd9fb0332b47fbf344f90ae8969
rye <59441604+rye-ndt@users.noreply.github.com>
2026-08-17 10:51:28 +0700

    Add: copy-linked-list-with-random-pointer - submission-12

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-12.go
@@ -0,0 +1,32 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Next *Node
+ *     Random *Node
+ * }
+ */
+
+func copyRandomList(head *Node) *Node {
+	runner := head 
+	cloneMap := map[*Node]*Node{}
+
+	for runner != nil {
+		cloneMap[runner] = &Node{ Val: runner.Val }
+		runner = runner.Next
+	}
+
+	result := cloneMap[head]
+	runner = result
+
+	for head != nil {
+		runner.Next = cloneMap[head.Next]
+		runner.Random = cloneMap[head.Random]
+
+		head = head.Next
+		runner = runner.Next
+	}
+
+
+	return result
+}
