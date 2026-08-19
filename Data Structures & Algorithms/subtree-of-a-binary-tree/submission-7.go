08dbc83c510a917f00031d3d8da057a453890ac3
rye <59441604+rye-ndt@users.noreply.github.com>
2026-08-19 10:17:43 +0700

    Add: subtree-of-a-binary-tree - submission-7

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-7.go
@@ -0,0 +1,18 @@
+func isSubtree(n, sub *TreeNode) bool {
+	var loop func(n, sub *TreeNode, flag bool) bool
+
+	loop = func(n, sub *TreeNode, flag bool) bool {
+		if (n == nil && sub != nil) || (n != nil && sub == nil) { return false }
+		if n == nil && sub == nil { return true }
+
+		match := n.Val == sub.Val && loop(n.Left, sub.Left, true) && loop(n.Right, sub.Right, true)
+
+		if !flag && !match { 
+			return loop(n.Left, sub, flag) || loop(n.Right, sub, flag)
+		}
+	
+		return match
+	}
+
+	return loop(n, sub, false)
+}
