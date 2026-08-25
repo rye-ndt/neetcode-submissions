96b02f46a97080990662449c4f2585bc080c5054
rye <59441604+rye-ndt@users.noreply.github.com>
2026-08-25 09:21:24 +0700

    Add: last-stone-weight - submission-4

---
+++ Data Structures & Algorithms/last-stone-weight/submission-4.go
@@ -0,0 +1,28 @@
+type IntHeap []int
+
+func (h IntHeap) Len() int { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Push(v any) { *h = append(*h, v.(int)) }
+func (h *IntHeap) Pop() any {
+	clone := *h 
+	last := clone[len(clone)-1]
+	*h = clone[:len(clone)-1]
+
+	return last
+}
+
+func lastStoneWeight(stones []int) int {
+	h := &IntHeap{}
+
+	for _, s := range stones {
+		heap.Push(h, s)
+	}
+
+	for h.Len() > 1 {
+		a, b := heap.Pop(h).(int), heap.Pop(h).(int)
+		heap.Push(h, a - b)
+	}
+
+	return h.Pop().(int)
+}
