7329e322a726fbe89b567cdcae36baa83abd4494
rye <59441604+rye-ndt@users.noreply.github.com>
2026-08-28 10:52:55 +0700

    Add: permutations - submission-7

---
+++ Data Structures & Algorithms/permutations/submission-7.go
@@ -0,0 +1,29 @@
+func permute(nums []int) [][]int {
+	sort.Ints(nums)
+	result := [][]int{}
+
+	var backtrack func(path []int) 
+	backtrack = func(path []int) {
+		if len(path) == len(nums) { 
+			result = append(result, append([]int{}, path...))
+			return 
+		}
+
+		for i := 0; i < len(nums); i++ {
+			if getSig(path)[nums[i]] { continue }
+			backtrack(append(path, nums[i]))
+		}
+	}
+
+	backtrack([]int{})
+
+	return result
+}
+
+func getSig(s []int) map[int]bool {
+	result := map[int]bool{}
+	for _, n := range s {
+		result[n] = true 
+	}
+	return result
+}
