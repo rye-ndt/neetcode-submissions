90dd79299e56692f0cb1673dc3d77986e1bca79f
rye <59441604+rye-ndt@users.noreply.github.com>
2026-08-28 10:25:05 +0700

    Add: combination-target-sum-ii - submission-8

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-8.go
@@ -0,0 +1,42 @@
+func combinationSum2(candidates []int, target int) [][]int {
+	sort.Ints(candidates)
+
+	result := [][]int{}
+
+	var backtrack func(path []int, index int) bool 
+	backtrack = func(path []int, index int) bool {
+
+		if reduce(path) == target {
+			result = append(result, append([]int{}, path...))
+			return true 
+		}
+
+		if reduce(path) > target { return false }
+
+		last := -1
+
+		for i := index; i < len(candidates); i++ {
+			if candidates[i] == last { continue }
+			path = append(path, candidates[i])
+			stored := backtrack(path, i+1) 
+			path = path[:len(path)-1]
+			if stored {
+				last = candidates[i]
+			}
+		}
+
+		return true
+	}
+
+	backtrack([]int{}, 0)
+
+	return result
+}
+
+func reduce(v []int) int {
+	result := 0
+	for _, a := range v {
+		result += a
+	}
+	return result 
+}
