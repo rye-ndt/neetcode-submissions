5bd2c6bebb4d3afea61250240d6d9312fc4b85de
rye <59441604+rye-ndt@users.noreply.github.com>
2026-09-03 09:34:58 +0700

    Add: subsets-ii - submission-11

---
+++ Data Structures & Algorithms/subsets-ii/submission-11.go
@@ -0,0 +1,33 @@
+func subsetsWithDup(nums []int) [][]int {
+	result := [][]int{}
+	sigs := map[string]bool{}
+
+	var backtrack func(path []int, curIndex int)
+	backtrack = func(path []int, curIndex int) {
+		cur := append([]int{}, path...)
+		sort.Ints(cur)
+
+		if found := sigs[signize(cur)]; !found {
+			sigs[signize(cur)] = true
+			result = append(result, cur)
+		} else {
+			return
+		}
+
+		for i := curIndex+1; i < len(nums); i++ {
+			path = append(path, nums[i])
+			backtrack(path, i)
+			path = path[:len(path)-1]
+		}
+	}
+
+	backtrack([]int{}, -1)
+
+	return result
+}
+
+func signize(nums []int) string {
+	total := ""
+	for _, n := range nums { total += fmt.Sprint(n) }
+	return total
+}
