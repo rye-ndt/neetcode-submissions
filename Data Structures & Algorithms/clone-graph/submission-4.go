aa2a51df597a01522ece1d03227284e4c408f97f
rye <59441604+rye-ndt@users.noreply.github.com>
2026-09-09 09:56:02 +0700

    Add: clone-graph - submission-4

---
+++ Data Structures & Algorithms/clone-graph/submission-4.go
@@ -0,0 +1,33 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Neighbors []*Node
+ * }
+ */
+
+func cloneGraph(node *Node) *Node {
+	clones := map[*Node]*Node{}
+
+	var clone func(n *Node) *Node 
+	clone = func(n *Node) *Node {
+		if n == nil { return nil }
+
+		if clones[n] != nil { return clones[n] }
+
+		newNode := &Node{ 
+			Val: n.Val,
+			Neighbors: []*Node{},
+		}
+
+		clones[n] = newNode
+
+		for _, c := range n.Neighbors {
+			newNode.Neighbors = append(newNode.Neighbors, clone(c))
+		}
+
+		return newNode
+	}
+	
+	return clone(node)
+}
