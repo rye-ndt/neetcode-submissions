c7566cf280f8646ee5397bb074f28be2084d83c0
rye <59441604+rye-ndt@users.noreply.github.com>
2026-09-14 09:23:34 +0700

    Add: climbing-stairs - submission-14

---
+++ Data Structures & Algorithms/climbing-stairs/submission-14.go
@@ -0,0 +1,12 @@
+func climbStairs(n int) int {
+	if n < 3 { return n }
+
+	steps := make([]int, n)
+	steps[0], steps[1] = 1, 2
+
+	for i := 2; i < n; i++ {
+		steps[i] = steps[i-1] + steps[i-2]
+	}
+
+	return steps[len(steps)-1]
+}
