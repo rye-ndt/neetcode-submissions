560097ceb703b31e2c5a01c6178ba62eb20a7c6b
rye <59441604+rye-ndt@users.noreply.github.com>
2026-09-23 10:29:30 +0700

    Add: redundant-connection - submission-3

---
+++ Data Structures & Algorithms/redundant-connection/submission-3.go
@@ -0,0 +1,42 @@
+func findRedundantConnection(edges [][]int) []int {
+	n := 0
+	graph := map[int]map[int]bool{}
+
+	for _, e := range edges {
+		if _, found := graph[e[0]]; !found { 
+			graph[e[0]] = map[int]bool{}
+		}
+
+		if _, found := graph[e[1]]; !found { 
+			graph[e[1]] = map[int]bool{}
+		}
+
+		graph[e[0]][e[1]] = true
+		graph[e[1]][e[0]] = true
+		n = max(n, max(e[0], e[1]))	
+	}
+
+	for i := len(edges)-1; i >= 0; i-- {
+		delete(graph[edges[i][0]], edges[i][1])
+		delete(graph[edges[i][1]], edges[i][0])
+		
+		walked := map[int]bool{}
+
+		var dfs func(cur int)
+		dfs = func(cur int){
+			walked[cur] = true
+			for option := range graph[cur] {
+				if !walked[option] { dfs(option) }
+			}
+		}
+
+		dfs(1)
+
+		if len(walked) == n { return edges[i] }
+
+		graph[edges[i][0]][edges[i][1]] = true
+		graph[edges[i][1]][edges[i][0]] = true
+	}
+
+	return []int{}
+}
